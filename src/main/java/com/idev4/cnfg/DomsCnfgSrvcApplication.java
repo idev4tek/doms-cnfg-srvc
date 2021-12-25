3e3620f8b7d936448bb824f4e3bc2a0e55359bdd
iDev4 Technologies <aurangz@gmail.com>
2021-12-26 04:15:25 +0500

    Initial commit

---
+++ src/main/java/com/idev4/cnfg/DomsCnfgSrvcApplication.java
@@ -0,0 +1,17 @@
+package com.idev4.cnfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class DomsCnfgSrvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomsCnfgSrvcApplication.class, args);
+    }
+
+}
